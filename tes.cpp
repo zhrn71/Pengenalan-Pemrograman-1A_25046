b011a07d4eabe68a039dd31de3e63ba1789f00eb
ran <zaufa8422@gmail.com>
2025-09-06 16:42:11 +0700

    Add files via upload
    soal 1

---
+++ tes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+string nama, usia;
+
+cout<<" masukkan nama anda: ";
+cin>>nama;
+
+cout<<" masukkan usia anda: ";
+cin>>usia;
+
+cout<<" Halo "<<nama<<", usia anda ";
+cout<< usia << "tahun"<< endl;
+
+return 0;
+}
